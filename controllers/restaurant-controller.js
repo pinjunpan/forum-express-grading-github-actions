f8a42602e34d81ce4b935666035553177656c69e
pinjunpan <pinjunpann@gmail.com>
2024-12-04 17:59:58 +0800

    feat: get user's favorited restaurants & switch button

--- controllers/restaurant-controller.js
+++ controllers/restaurant-controller.js
@@ -26,9 +26,12 @@ const restaurantController = {
       Category.findAll({ raw: true })
     ])
       .then(([restaurants, categories]) => {
+        const favoritedRestaurantsId = req.user && req.user.FavoritedRestaurants.map(fr => fr.id)
+
         const data = restaurants.rows.map(r => ({
           ...r,
-          description: r.description.substring(0, DEFAULT_DESCRIPTION_LENGTH)
+          description: r.description.substring(0, DEFAULT_DESCRIPTION_LENGTH),
+          isFavorited: favoritedRestaurantsId.includes(r.id)
         }))
 
         return res.render('restaurants', {
@@ -44,7 +47,8 @@ const restaurantController = {
     return Restaurant.findByPk(req.params.id, {
       include: [
         Category,
-        { model: Comment, include: User }
+        { model: Comment, include: User },
+        { model: User, as: 'FavoritedUsers' }
       ],
       order: [['createdAt', 'DESC']]
     })
@@ -54,7 +58,12 @@ const restaurantController = {
         return restaurant.increment('viewCounts')
       })
       .then(restaurant => {
-        res.render('restaurant', { restaurant: restaurant.toJSON() })
+        const isFavorited = restaurant.FavoritedUsers.some(f => f.id === req.user.id)
+
+        res.render('restaurant', {
+          restaurant: restaurant.toJSON(),
+          isFavorited
+        })
       })
       .catch(err => next(err))
   },
