4bf273230b09a04c9cea52c48a3bc3e4bd91ded7
pinjunpan <pinjunpann@gmail.com>
2024-12-05 15:22:55 +0800

    rebase: R04-add like/unlike feature
    fix conflict

--- controllers/restaurant-controller.js
+++ controllers/restaurant-controller.js
@@ -27,11 +27,13 @@ const restaurantController = {
     ])
       .then(([restaurants, categories]) => {
         const favoritedRestaurantsId = req.user && req.user.FavoritedRestaurants.map(fr => fr.id)
+        const likedRestaurantsId = req.user && req.user.LikedRestaurants.map(lr => lr.id)
 
         const data = restaurants.rows.map(r => ({
           ...r,
           description: r.description.substring(0, DEFAULT_DESCRIPTION_LENGTH),
-          isFavorited: favoritedRestaurantsId.includes(r.id)
+          isFavorited: favoritedRestaurantsId.includes(r.id),
+          isLiked: likedRestaurantsId.includes(r.id)
         }))
 
         return res.render('restaurants', {
@@ -48,7 +50,8 @@ const restaurantController = {
       include: [
         Category,
         { model: Comment, include: User },
-        { model: User, as: 'FavoritedUsers' }
+        { model: User, as: 'FavoritedUsers' },
+        { model: User, as: 'LikedUsers' }
       ],
       order: [['createdAt', 'DESC']]
     })
@@ -59,10 +62,12 @@ const restaurantController = {
       })
       .then(restaurant => {
         const isFavorited = restaurant.FavoritedUsers.some(f => f.id === req.user.id)
+        const isLiked = restaurant.LikedUsers.some(l => l.id === req.user.id)
 
         res.render('restaurant', {
           restaurant: restaurant.toJSON(),
-          isFavorited
+          isFavorited,
+          isLiked
         })
       })
       .catch(err => next(err))
